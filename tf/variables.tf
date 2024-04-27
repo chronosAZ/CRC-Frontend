cccc0ebbf5659893d83b8bae7c5aef657d7dbc23
Brian Haas <bwhaas@gmail.com>
2024-04-27 16:20:31 -0700

    Changed to new domain

--- tf/variables.tf
+++ tf/variables.tf
@@ -3,13 +3,9 @@ variable "region" {
   description = "The AWS Region to use"
   default     = "us-east-1"
 }
-variable "bucket_prefix" {
-  type        = string
-  description = "The prefix for the S3 bucket"
-  default     = "crc-website"
-}
+
 variable "domain_name" {
   type        = string
   description = "The domain name to use"
-  default     = "musing-brian.com"
+  default     = "brianwhaas.com"
 }
